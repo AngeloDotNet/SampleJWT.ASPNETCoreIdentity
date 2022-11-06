8ca082faf2f85cf7bb1b6bfa3b7b87442f1d5d1f
Angelo Pirola <angelo9871@gmail.com>
2022-11-06 20:43:44 +0100

    Refactoring Models

--- src/Identity.Models/UserResponse.cs
+++ src/Identity.Models/UserResponse.cs
@@ -2,10 +2,9 @@
 {
     public class UserResponse
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
-
         public string Email { get; set; }
     }
 }
